59e41ef98940a4648fa4c6e104a860c3c62d6655
2711-bharath <2711bharath@gmail.com>
2021-12-16 11:49:34 +0530

    fixed bug create your blog

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+import { AppUser } from 'src/app/models/appuser';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  appUser: AppUser;
 
-  constructor() { }
+  constructor(private authService: AuthService, private route: Router) { }
 
   ngOnInit(): void {
+    this.authService.appUser$.subscribe(appUser => this.appUser = appUser);
+  }
+
+  openLogin(): void {
+    if (this.appUser) {
+      this.route.navigateByUrl('/addpost');
+    } else {
+      this.authService.login();
+    }
   }
 
 }
